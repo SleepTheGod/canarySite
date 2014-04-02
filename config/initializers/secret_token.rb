d4d0d8afd5091b9d44729cf331929e68034428fb
adamrdavid <adamrdavid@gmail.com>
2014-04-01 21:34:58 -0700

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CanarySite::Application.config.secret_key_base = 'c085f31de7ddd11ecd2f8e985eb622b0b8057003f67b27aef8a32d91de9045591146a70edeac3a5dbff5a2e70ae9fd0995570a7e69de02caa9be50f511c24d57'
